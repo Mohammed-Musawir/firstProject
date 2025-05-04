6242fa2992bc9ff9157cacb4c28778c8ca283e56
Mohammed Musawir <munnugaming48@gmail.com>
2025-05-04 16:29:38 +0530

    small

--- config/multer.js
+++ config/multer.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); 
   }
-});
+})
 
 
 const fileFilter = (req, file, cb) => {
